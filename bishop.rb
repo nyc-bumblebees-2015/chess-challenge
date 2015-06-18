e4ea1df55effed144c95ea69953d062b239c63cd
Deren Dogrusoy & britneywright & Jonathan Chung <pair+aderend.britneywright.hukashi@devbootcamp.com>
2015-06-18 17:09:43 -0400

    write move? method for Bishop

--- bishop.rb
+++ bishop.rb
@@ -4,6 +4,22 @@ class Bishop
     @marker = "B"
   end
 
-
-
+  def move?(origin, destination)
+    if @board[MAP_HASH.key(destination)] != nil && @board[MAP_HASH.key([x,y])].color == @color
+      return false
+    end
+    horizontal = (origin[0] - destination[0]  < 0 ? -1 : 1)
+    vertical = (origin[1] - destination[1] < 0 ? -1 : 1)
+    distance = (origin[0] - destination[0]).abs
+    x = origin[0]
+    y = origin[1]
+    distance-1.times do |x, y|
+      x+= horizontal
+      y+= vertical
+      if @board[MAP_HASH.key([x,y])] != nil
+        return false
+      end
+      return true
+    end
+  end
 end
