7b2f4ec978f39562aba14990fec031d195c446fe
Deren Dogrusoy & Jonathan Chung <pair+aderend.hukashi@devbootcamp.com>
2015-06-19 11:43:04 -0400

    fixed comments

--- bishop.rb
+++ bishop.rb
@@ -31,24 +31,4 @@ class Bishop
     end
       return true  
   end
-
-
-    # if board[MAP_HASH.key(destination)] != nil && board[MAP_HASH.key(destination)].color == @color
-    #   return false
-    # end
-    # horizontal = (origin[0] - destination[0]  < 0 ? -1 : 1)
-    # vertical = (origin[1] - destination[1] < 0 ? -1 : 1)
-    # distance = (origin[0] - destination[0]).abs
-    # x = origin[0]
-    # y = origin[1]
-    # until distance == 1 do
-    #   y += horizontal
-    #   x += vertical
-    #   if board[MAP_HASH.key([x,y])] != nil
-    #     return false
-    #   end
-    #   distance -= 1
-    # end
-    # return true
-  end
 end
