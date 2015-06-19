b7680eab3e72b71494867d2e3c75ea7b89591508
v <vicxizhu@gmail.com>
2015-06-19 12:21:26 -0400

    add player coordinate validation and convertion methods

--- game.rb
+++ game.rb
@@ -1,10 +1,12 @@
 require_relative 'board'
 require_relative 'view'
+require 'pry'
 
 class Chess
-
+  DIMENSION = 8
   def initialize(board)
     @board = board
+    binding.pry
     run
   end
 
@@ -30,11 +32,27 @@ class Chess
 
   end
 
-
-  def input_convert(input)
-    input.to_i
+  #private
+  def valid_coords
+    rows = ('a'..'z').to_a[0...DIMENSION] * DIMENSION
+    cols = ('1'..'26').to_a[0...DIMENSION].map { |col_num| col_num * DIMENSION }.map(&:chars).flatten
+    rows.zip(cols).map(&:join)
   end
 
+  def valid_coord?(coord)
+    valid_coords.include?(coord)
+  end
+
+  def coord_key_convert(coord)
+    row_keys = ('a'..'z').to_a[0...DIMENSION]
+    row_values = (1..DIMENSION).to_a
+
+    row_key_value_pairs = row_keys.zip(row_values)
+    row_hash = row_key_value_pairs.each_with_object({}) { |pairs, hash| hash[pairs[0]] = pairs[-1] }
+    binding.pry
+    row, col = coord.downcase.chars
+    "#{row_hash[row]}#{col}".to_i
+  end
 
 end
 
